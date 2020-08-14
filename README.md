412f30513f0b4c93cb26d937517a9fce32164590
no0g <tp048892@mail.apu.edu.my>
2020-08-14 15:25:47 +0800

    added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with Quiche
+Disclaimer: This script is trash, only work for ubuntu/debian or anything with apt.   
+## Just run it
+Yes, You download it, you run it, it will do everything, it doesn't check, it runs.   
+I made this because i'm tired of doing it again and again.   
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install rustup
+echo "Installing rustup"
+sudo snap install rustup --classic
+rustup install stable
+sudo rustup install stable
+echo "rustup installed"
+# Install cmake
+echo "Installing cmake"
+sudo apt install cmake
+echp "cmake installed"
+# Clone Quiche
+echo "Cloning quiche to /opt"
+sudo git clone --recursive https://github.com/cloudflare/quiche /opt/quiche
+cd /opt/quiche
+echo "Building quiche"
+sudo cargo build --examples
+echo "done"
+
+echo "You can use this command to send request"
+echo "sudo cargo run --manifest-path=/opt/quiche/tools/apps/Cargo.toml --bin quiche-client -- https://<yourwebsite>"
+
+
